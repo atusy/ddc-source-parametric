744bc8658d5d1e508d433565be738cf890568c70
atusy <30277794+atusy@users.noreply.github.com>
2025-04-11 22:47:14 +0900

    chore: update deps

--- denops/@ddc-sources/parametric.ts
+++ denops/@ddc-sources/parametric.ts
@@ -1,28 +1,28 @@
-import {
-  BaseSource,
-  DdcOptions,
-  Item,
-  SourceOptions,
-} from "https://deno.land/x/ddc_vim@v4.1.0/types.ts";
-import { Denops } from "https://deno.land/x/ddc_vim@v4.1.0/deps.ts";
+import type { Denops } from "jsr:@denops/core@~7.0.0";
+import { BaseSource } from "jsr:@shougo/ddc-vim@~9.4.0/source";
+import type {
+	DdcOptions,
+	Item,
+	SourceOptions,
+} from "jsr:@shougo/ddc-vim@~9.4.0/types";
 
 type Params = {
-  items: Item[];
+	items: Item[];
 };
 
 export class Source extends BaseSource<Params> {
-  override gather(args: {
-    denops: Denops;
-    options: DdcOptions;
-    sourceOptions: SourceOptions;
-    sourceParams: Params;
-    completeStr: string;
-  }): Promise<Item[]> {
-    const items = args.sourceParams.items;
-    return Promise.resolve(items);
-  }
+	override gather(args: {
+		denops: Denops;
+		options: DdcOptions;
+		sourceOptions: SourceOptions;
+		sourceParams: Params;
+		completeStr: string;
+	}): Promise<Item[]> {
+		const items = args.sourceParams.items;
+		return Promise.resolve(items);
+	}
 
-  override params(): Params {
-    return { items: [] };
-  }
+	override params(): Params {
+		return { items: [] };
+	}
 }
